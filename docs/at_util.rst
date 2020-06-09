d9a8b5ed869511bbd18c0843af5f361ffb820a44
Bin Yang <bin.yang@algotune.com>
2020-06-09 18:10:44 -0400

    first commit

---
+++ docs/at_util.rst
@@ -0,0 +1,10 @@
+at_util Package
+==================================
+
+:mod:`at_util` Module
+----------------------------------------
+
+.. automodule:: at_util.at_util
+    :members:
+    :undoc-members:
+    :show-inheritance:
---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = []
+setup_requirements = ['pytest-runner']
+
+test_requirements = ['pytest>=3']
+
+setup(
+    author="Bin Yang",
+    author_email='bin.yang@algotune.com',
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="at_util",
+    install_requires=requirements,
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='at_util',
+    name='at_util',
+    packages=find_packages(include=['at_util', 'at_util.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/algotune/at_util',
+    version='0.0.1',
+    zip_safe=False,
+)
